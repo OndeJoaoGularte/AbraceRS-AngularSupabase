058e9118b4056b3d33887d4ef4f783ad793e66a7
João Gularte <joaomgularte5555@gmail.com>
2025-08-20 11:32:00 -0300

    blog

--- src/app/pages/blog/blog.ts
+++ src/app/pages/blog/blog.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { PostsList } from "../../components/posts/posts-list/posts-list";
+import { Component, OnInit } from '@angular/core';
+import { PostsList } from '../../components/posts/posts-list/posts-list';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Posts } from '../../services/posts/posts';
 
 @Component({
   selector: 'app-blog',
-  imports: [PostsList],
+  imports: [CommonModule, RouterModule, PostsList],
   templateUrl: './blog.html',
-  styleUrl: './blog.scss'
+  styleUrl: './blog.scss',
 })
-export class Blog {
+export class Blog implements OnInit {
+  featuredPost: any = null;
+  loading = true;
 
+  constructor(private postsService: Posts) {}
+
+  async ngOnInit(): Promise<void> {
+    const allPosts = await this.postsService.getPosts('publish-desc', ''); // puxa todos os posts ordenado pelo mais recente
+
+    if (allPosts && allPosts.length > 0) {
+      this.featuredPost = allPosts[0]; // mostra o post mais recente
+    }
+
+    this.loading = false;
+  }
 }
