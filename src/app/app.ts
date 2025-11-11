255d4d4ba6b2b48fcb9c3f4b8027ccc8c00c582c
João Gularte <joaomgularte5555@gmail.com>
2025-11-10 22:29:04 -0300

    teste

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,38 +10,8 @@ import { Supabase } from './services/supabase/supabase';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
+export class App {
   protected readonly title = signal('abracers-angular-supabase');
 
   constructor(private supabaseService: Supabase) {}
-
-  async ngOnInit(): Promise<void> {
-    console.log('Buscando dados no Supabase...');
-    try {
-      const { data, error } = await this.supabaseService.client
-        .from('projects')
-        .select('*');
-
-      if (error) {
-        throw error;
-      }
-
-      console.log('Dados recebidos:', data);
-    } catch (error) {
-      console.error('Erro ao buscar dados:', error);
-    }
-    try {
-      const { data, error } = await this.supabaseService.client
-        .from('posts')
-        .select('*');
-
-      if (error) {
-        throw error;
-      }
-
-      console.log('Dados recebidos:', data);
-    } catch (error) {
-      console.error('Erro ao buscar dados:', error);
-    }
-  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://ldgfntfjwpdlyxryjglo.supabase.co',
-  supabaseKey: 'sb_publishable_O_TbLrMUz0B5m_8RMVRpkQ_3dEYRrTF'
+  supabaseUrl: 'NG_APP_SUPABASE_URL',
+  supabaseKey: 'NG_APP_SUPABASE_KEY'
 };
