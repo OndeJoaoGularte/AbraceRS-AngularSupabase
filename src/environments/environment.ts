14734d4ee2b60c1224a1923f46e8c2a3f139bc5b
João Gularte <joaomgularte5555@gmail.com>
2025-11-10 21:18:02 -0300

    variáveis de ambiente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://ldgfntfjwpdlyxryjglo.supabase.co',
-  supabaseKey: 'sb_publishable_O_TbLrMUz0B5m_8RMVRpkQ_3dEYRrTF'
+  supabaseUrl: process.env['NG_APP_SUPABASE_URL'],
+  supabaseKey: process.env['NG_APP_SUPABASE_KEY']
 };
+
+//supabaseUrl: 'https://ldgfntfjwpdlyxryjglo.supabase.co',
+//supabaseKey: 'sb_publishable_O_TbLrMUz0B5m_8RMVRpkQ_3dEYRrTF'
