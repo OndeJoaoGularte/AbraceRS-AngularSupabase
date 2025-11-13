8dff48428e83d05e18188ae59527e3aa608bbad2
João Gularte <joaomgularte5555@gmail.com>
2025-11-13 20:53:52 -0300

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  supabaseUrl: process.env['NG_APP_SUPABASE_URL'],
-  supabaseKey: process.env['NG_APP_SUPABASE_KEY']
+  supabaseUrl: 'https://ldgfntfjwpdlyxryjglo.supabase.co',
+  supabaseKey: 'sb_publishable_O_TbLrMUz0B5m_8RMVRpkQ_3dEYRrTF'
 };
