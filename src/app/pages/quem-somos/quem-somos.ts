bb3ef9e8a49347bf8871770134a75c1424fe86c7
João Gularte <107907952+OndeJoaoGularte@users.noreply.github.com>
2026-07-11 18:50:29 -0300

    ajustes

--- src/app/pages/quem-somos/quem-somos.ts
+++ src/app/pages/quem-somos/quem-somos.ts
@@ -10,7 +10,7 @@ export class QuemSomos {
   teamMembers = [
     { name: 'Marcelo Gularte', role: 'Presidente', image: 'assets/images/quemsomos/equipe/marcelo.jpg' },
     { name: 'Benny P. Dorneles', role: 'Vice-Presidente', image: 'assets/images/quemsomos/equipe/benny.jpg' },
-    { name: 'Camila Borelli', role: 'Diretora Financeira', image: 'assets/images/quemsomos/equipe/camila.jpg' },
+    { name: 'Henrique Chavaré', role: 'Diretor Financeiro', image: 'assets/images/quemsomos/equipe/henrique.jpeg' },
     { name: 'Alexandre de Oliveira', role: 'Diretor de Infraestrutura', image: 'assets/images/quemsomos/equipe/alexandre.jpg' },
     { name: 'Shaiane Tessmann', role: 'Diretora Técnica', image: 'assets/images/quemsomos/equipe/shaiane.jpg' },
     { name: 'Rosane Casanova', role: 'Diretora de Ajuda Humanitária', image: 'assets/images/quemsomos/equipe/rosane.png' }
